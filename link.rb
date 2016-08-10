7f39f8c5adc9ea918763352a5ba4456c4dca9d76
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-08-10 12:53:48 +0300

    post class + ancestors

--- link.rb
+++ link.rb
@@ -1,10 +1,11 @@
 
 class Link < Post
+	# Класс для сохранения ссылок
 
 	def initialize
 		super #внутри метода дочернего класса означает, дерни метод з таким же названием в родителя
 
-		@url = nil
+		@url = ''
 	end
 
 	def read_from_console
